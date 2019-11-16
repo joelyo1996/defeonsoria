1883bb4b0e7ba0281c377f0714fe3751ca44f3c4
joel <joel1996bello@gmail.com>
2019-11-16 09:19:53 -0300

    sa

--- menu2.gd
+++ menu2.gd
@@ -1,7 +1,7 @@
 extends Node2D
-
+signal area_entere
 # Declare member variables here. Examples:
-# var a = 2
+var a = 2
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
@@ -9,11 +9,39 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+
+		#$termo.position = get_global_mouse_position()
+	
 #	pass
 
 
 func _on_Button_pressed():
 	print("hola")
 	$AnimationPlayer.play("varraojetos")
+	pass # Replace with function body.
+
+
+func _on_termo_mouse_entered():
+	
+	pass # Replace with function body.
+
+
+func _on_Area2D_area_entered(area):
+	print("hola")
+	var a=1
+	pass # Replace with function body.
+
+
+func _on_Button2_pressed():
+	$termo2.play("termo")
+	pass # Replace with function body.
+
+
+func _on_votomcuadero_pressed():
+	$amicuadermo.play("cuadermo")
+	pass # Replace with function body.
+
+
+func _on_votompem_pressed():
+	$Animapem.play("pem")
 	pass # Replace with function body.
