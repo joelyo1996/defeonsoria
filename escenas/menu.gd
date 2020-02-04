79e185601b1e20a8c83e7f3e2aa68f555b1f8b8c
joelyo1996 <38585894+joelyo1996@users.noreply.github.com>
2020-02-04 17:56:33 -0300

    martes

--- escenas/menu.gd
+++ escenas/menu.gd
@@ -6,13 +6,36 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	
+	yield(get_tree().create_timer(1),"timeout")
+	$AnimationPlayer.play("precentacion")
+	yield(get_tree().create_timer(4),"timeout")
+	$AnimationPlayer.play("animacion_sc")
+	yield(get_tree().create_timer(3),"timeout")
+	$ColorRect.queue_free()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	pass 
+
+
+func _process(delta):
+	$fondo/sol.rotation =+ 5
+
 
 
 func _on_play_pressed():
 	get_tree().change_scene("res://primer_escena_el_conedor.tscn")
 	
+
+
+func _on_salir_pressed():
+	yield(get_tree().create_timer(1),"timeout")
+	$AnimationPlayer.play("salir")
+	yield(get_tree().create_timer(3),"timeout")
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_info_pressed():
+	yield(get_tree().create_timer(1),"timeout")
+	$AnimationPlayer.play("credito")
+	pass # Replace with function body.
